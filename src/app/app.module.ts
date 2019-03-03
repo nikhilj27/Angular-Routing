26cdbaf3cae0c7bfec7e5b9eb6fc1c1548824996
Nikhil Jadhav <nikhil.jadhav1527@gmail.com>
2019-03-03 09:37:17 +0530

    Basic routing full example.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { ProtectedComponent } from './protected/protected.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent,
+    ProductsComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
